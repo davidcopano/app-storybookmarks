6569814c03f2af7ca88843201cca9855fb136a8e
David Copano <davidcopano96@gmail.com>
2020-06-21 18:17:40 +0200

    Añadido plugin traducciones, haciendo splash

--- src/app/pages/tutorial/tutorial.module.ts
+++ src/app/pages/tutorial/tutorial.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
+import { TranslateModule } from '@ngx-translate/core';
 import { TutorialPageRoutingModule } from './tutorial-routing.module';
-
 import { TutorialPage } from './tutorial.page';
 
 @NgModule({
@@ -13,7 +11,8 @@ import { TutorialPage } from './tutorial.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    TutorialPageRoutingModule
+    TutorialPageRoutingModule,
+    TranslateModule
   ],
   declarations: [TutorialPage]
 })
