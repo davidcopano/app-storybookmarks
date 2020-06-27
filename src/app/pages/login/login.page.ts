c2e57ff9aca7a2ce5fab24b1a68813b85296d01d
David Copano <davidcopano96@gmail.com>
2020-06-27 16:46:03 +0200

    Haciendo login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
+  @ViewChild('passwordInput', { read: ElementRef }) passwordEye: ElementRef;
+
+  passwordTypeInput = 'password';
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  togglePasswordMode() {
+    this.passwordTypeInput = this.passwordTypeInput === 'text' ? 'password' : 'text';
+    const nativeEl = this.passwordEye.nativeElement.querySelector('input');
+    const inputSelection = nativeEl.selectionStart;
+    nativeEl.focus();
+    setTimeout(() => {
+      nativeEl.setSelectionRange(inputSelection, inputSelection);
+    }, 1);
+  }
 }
