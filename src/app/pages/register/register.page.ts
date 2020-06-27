a3fdfb7ed931b4d5e0f604234e736ba2e4c07d95
David Copano <davidcopano96@gmail.com>
2020-06-27 17:35:25 +0200

    Maquetado registro

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
+  @ViewChild('passwordInput', { read: ElementRef }) passwordEye: ElementRef;
+
+  passwordTypeInput = 'password';
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  togglePasswordMode() {
+    this.passwordTypeInput = this.passwordTypeInput === 'text' ? 'password' : 'text';
+    const nativeEl = this.passwordEye.nativeElement.querySelector('input');
+    const inputSelection = nativeEl.selectionStart;
+    nativeEl.focus();
+    setTimeout(() => {
+      nativeEl.setSelectionRange(inputSelection, inputSelection);
+    }, 1);
+  }
 }
