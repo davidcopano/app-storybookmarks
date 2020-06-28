36aeb6d82d2b7ea1019ddf23ce5f0b65e6e9ec05
David Copano <davidcopano96@gmail.com>
2020-06-28 14:56:14 +0200

    Haciendo desarrollo login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -9,11 +10,21 @@ export class LoginPage implements OnInit {
 
   @ViewChild('passwordInput', { read: ElementRef }) passwordEye: ElementRef;
 
-  passwordTypeInput = 'password';
+  private passwordTypeInput = 'password';
+  private form: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  submitForm() {
+    console.log('submitForm()');
+    console.log(this.form.value);
   }
 
   togglePasswordMode() {
