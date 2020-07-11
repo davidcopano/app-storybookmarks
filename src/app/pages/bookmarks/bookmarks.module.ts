0bb9adf670532317fd972a792b8685f193aae526
David Copano <davidcopano96@gmail.com>
2020-07-11 13:01:04 +0200

    Maquetando marcadores

--- src/app/pages/bookmarks/bookmarks.module.ts
+++ src/app/pages/bookmarks/bookmarks.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
+import { TranslateModule } from '@ngx-translate/core';
 import { BookmarksPageRoutingModule } from './bookmarks-routing.module';
-
 import { BookmarksPage } from './bookmarks.page';
 
 @NgModule({
@@ -13,7 +11,8 @@ import { BookmarksPage } from './bookmarks.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    BookmarksPageRoutingModule
+    BookmarksPageRoutingModule,
+    TranslateModule
   ],
   declarations: [BookmarksPage]
 })
