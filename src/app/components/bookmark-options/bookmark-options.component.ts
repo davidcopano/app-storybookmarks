82ccbe34ca483fe02233a87614f0100c87ab71dc
David Copano <davidcopano96@gmail.com>
2020-07-11 14:58:57 +0200

    Maquetando marcadores

--- src/app/components/bookmark-options/bookmark-options.component.ts
+++ src/app/components/bookmark-options/bookmark-options.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Bookmark } from '../../interfaces';
 import { NavParams } from '@ionic/angular';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
 @Component({
   selector: 'app-bookmark-options',
@@ -11,10 +12,13 @@ export class BookmarkOptionsComponent implements OnInit {
 
   item: Bookmark;
 
-  constructor(private navParams: NavParams) { }
+  constructor(private navParams: NavParams, private inAppBrowser: InAppBrowser) { }
 
   ngOnInit() {
     this.item = this.navParams.get('item');
   }
 
+  openLink() {
+    this.inAppBrowser.create(this.item.url, '_system');
+  }
 }
