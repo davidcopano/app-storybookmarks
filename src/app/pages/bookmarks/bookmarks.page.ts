69738381a3c81641283a3167d86d2171d44d0412
David Copano <davidcopano96@gmail.com>
2020-07-12 17:44:14 +0200

    Maquetando marcadores

--- src/app/pages/bookmarks/bookmarks.page.ts
+++ src/app/pages/bookmarks/bookmarks.page.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Bookmark } from '../../interfaces';
+import { IonInfiniteScroll } from '@ionic/angular';
 
 @Component({
   selector: 'app-bookmarks',
@@ -8,13 +9,15 @@ import { Bookmark } from '../../interfaces';
 })
 export class BookmarksPage implements OnInit {
 
+  @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
+
   bookmarks: Bookmark[] = [];
   private bookmarksOrder: string = 'default';
 
   constructor() { }
 
   ngOnInit() {
-    for(let i = 1; i <= 5; i++) {
+    for (let i = 1; i <= 5; i++) {
       this.bookmarks.push({
         id: i.toString(),
         color: 'red',
@@ -31,4 +34,28 @@ export class BookmarksPage implements OnInit {
     }
   }
 
+  loadMoreBookmarks($event) {
+    setTimeout(() => {
+      for (let i = 1; i <= 5; i++) {
+        this.bookmarks.push({
+          id: i.toString(),
+          color: 'red',
+          created_at: new Date().toISOString(),
+          folder_id: null,
+          public: false,
+          title: `Carpeta ${i}`,
+          url: 'https://www.google.es',
+          user_id: 2,
+          expiration_date: null,
+          note: null,
+          tag_id: null
+        });
+      }
+      $event.target.complete();
+
+      if(this.bookmarks.length == 50) {
+        this.infiniteScroll.disabled = true;
+      }
+    }, 500);
+  }
 }
