5811d77b81027120301b58b08b945ed690a85e56
David Copano <davidcopano96@gmail.com>
2020-07-14 14:28:55 +0200

    Maquetando editar marcador

--- src/app/pages/edit-bookmark/edit-bookmark.page.ts
+++ src/app/pages/edit-bookmark/edit-bookmark.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Bookmark } from '../../interfaces';
 
 @Component({
   selector: 'app-edit-bookmark',
@@ -8,18 +9,20 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class EditBookmarkPage implements OnInit {
 
+  bookmark: Bookmark;
   form: FormGroup;
 
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.bookmark = history.state.bookmark;
     this.form = this.formBuilder.group({
-      title: ['', Validators.required],
-      url: ['', Validators.required],
-      color: ['#000000', Validators.required],
-      note: [''],
-      folder_id: [''],
-      public: [''],
+      title: [this.bookmark.title, Validators.required],
+      url: [this.bookmark.url, Validators.required],
+      color: [this.bookmark.color, Validators.required],
+      note: [this.bookmark.note],
+      folder_id: [this.bookmark.folder_id],
+      public: [this.bookmark.public],
       expiration_date: [new Date().toISOString()]
     })
   }
