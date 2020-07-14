b9757943d27961d1d87b7a85068971a3ab2a6705
David Copano <davidcopano96@gmail.com>
2020-07-14 15:15:13 +0200

    Maquetando carpetas

--- src/app/components/folder-options/folder-options.component.ts
+++ src/app/components/folder-options/folder-options.component.ts
@@ -3,7 +3,7 @@ import { forkJoin } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { TranslateService } from '@ngx-translate/core';
 import { Folder } from 'src/app/interfaces';
-import { NavParams, NavController } from '@ionic/angular';
+import { NavParams, NavController, PopoverController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-folder-options',
@@ -20,8 +20,10 @@ export class FolderOptionsComponent implements OnInit {
   private cancelText: string;
   private editDataText: string;
   private deleteText: string;
+  private deleteFolderText: string;
+  private deleteFolderConfirmationText: string;
 
-  constructor(private navCtrl: NavController, private navParams: NavParams, private translateService: TranslateService) { }
+  constructor(private navCtrl: NavController, private navParams: NavParams, private translateService: TranslateService, private popoverCtrl: PopoverController, private alertCtrl: AlertController) { }
 
   ngOnInit() {
     this.item = this.navParams.get('item');
@@ -33,7 +35,36 @@ export class FolderOptionsComponent implements OnInit {
       this.cancelText = translations.cancelText;
       this.editDataText = translations.editDataText;
       this.deleteText = translations.deleteText;
+      this.deleteFolderText = translations.deleteFolderText;
+      this.deleteFolderConfirmationText = translations.deleteFolderConfirmationText;
     });
+  }
+
+  async delete() {
+    let alert = await this.alertCtrl.create({
+      header: this.deleteFolderText,
+      message: this.deleteFolderConfirmationText,
+      buttons: [
+        {
+          text: this.cancelText,
+          role: 'cancel'
+        },
+        {
+          text: this.yesDeleteText,
+          role: 'destructive',
+          cssClass: 'text-danger',
+          handler: () => {
+            console.log('delete clicked');
+            this.closeSelf();
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+  private closeSelf() {
+    return this.popoverCtrl.dismiss();
   }
 
   private getTranslationValues() {
@@ -44,15 +75,19 @@ export class FolderOptionsComponent implements OnInit {
       this.translateService.get('CANCEL'),
       this.translateService.get('EDIT_DATA'),
       this.translateService.get('DELETE'),
+      this.translateService.get('DELETE_FOLDER'),
+      this.translateService.get('DELETE_FOLDER_CONFIRMATION'),
     ).pipe(
-      map(([viewText, bookmarksText, yesDeleteText, cancelText, editDataText, deleteText]) => {
+      map(([viewText, bookmarksText, yesDeleteText, cancelText, editDataText, deleteText, deleteFolderText, deleteFolderConfirmationText]) => {
         return {
           viewText,
           bookmarksText,
           yesDeleteText,
           cancelText,
           editDataText,
-          deleteText
+          deleteText,
+          deleteFolderText,
+          deleteFolderConfirmationText
         };
       })
     );
