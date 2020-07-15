f24f73805d2100010f4bfaacb6f30e5ed22d34b2
David Copano <davidcopano96@gmail.com>
2020-07-15 17:35:28 +0200

    Maquetado ver marcadores de carpeta

--- src/app/pages/view-folder-bookmarks/view-folder-bookmarks.module.ts
+++ src/app/pages/view-folder-bookmarks/view-folder-bookmarks.module.ts
@@ -1,20 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
+import { TranslateModule } from '@ngx-translate/core';
 import { ViewFolderBookmarksPageRoutingModule } from './view-folder-bookmarks-routing.module';
-
 import { ViewFolderBookmarksPage } from './view-folder-bookmarks.page';
+import { BookmarkComponent } from 'src/app/components/bookmark/bookmark.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    ViewFolderBookmarksPageRoutingModule
+    ViewFolderBookmarksPageRoutingModule,
+    TranslateModule
   ],
-  declarations: [ViewFolderBookmarksPage]
+  entryComponents: [BookmarkComponent],
+  declarations: [ViewFolderBookmarksPage, BookmarkComponent]
 })
 export class ViewFolderBookmarksPageModule {}
--- src/app/pages/view-folder-bookmarks/view-folder-bookmarks.page.ts
+++ src/app/pages/view-folder-bookmarks/view-folder-bookmarks.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
+import { Bookmark } from 'src/app/interfaces';
 
 @Component({
   selector: 'app-view-folder-bookmarks',
@@ -7,9 +9,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewFolderBookmarksPage implements OnInit {
 
+  @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
+
+  bookmarks: Bookmark[] = [];
+
   constructor() { }
 
   ngOnInit() {
+    for (let i = 1; i <= 5; i++) {
+      this.bookmarks.push({
+        id: i.toString(),
+        color: 'red',
+        created_at: new Date().toISOString(),
+        folder_id: null,
+        public: false,
+        title: `Carpeta ${i}`,
+        url: 'https://www.google.es',
+        user_id: 2,
+        expiration_date: null,
+        note: null,
+        tag_id: null
+      });
+    }
+  }
+
+  loadMoreBookmarks($event) {
+    setTimeout(() => {
+      for (let i = 1; i <= 5; i++) {
+        this.bookmarks.push({
+          id: i.toString(),
+          color: 'red',
+          created_at: new Date().toISOString(),
+          folder_id: null,
+          public: false,
+          title: `Carpeta ${i}`,
+          url: 'https://www.google.es',
+          user_id: 2,
+          expiration_date: null,
+          note: null,
+          tag_id: null
+        });
+      }
+      $event.target.complete();
+
+      if (this.bookmarks.length == 50) {
+        this.infiniteScroll.disabled = true;
+      }
+    }, 500);
   }
 
 }
