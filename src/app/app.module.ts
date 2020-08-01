5906c6a0a8bd97b9d53bb94dd23e9fbff876d23e
David Copano <davidcopano96@gmail.com>
2020-08-01 17:23:40 +0200

    Haciendo cargar iframe con un pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,13 +18,14 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { SafePipe } from './pipes/safe/safe.pipe';
 
 export function createTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SafePipe],
   entryComponents: [],
   imports: [
     BrowserModule,
