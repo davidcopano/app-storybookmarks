da98e2c14cab2ada98cf3b1c0c4892c84ff9e4b6
David Copano <davidcopano96@gmail.com>
2020-08-08 19:52:24 +0200

    Documentando código del proyecto y mejoras

--- src/app/validators/index.ts
+++ src/app/validators/index.ts
@@ -3,5 +3,5 @@ import { FormGroup } from '@angular/forms';
 export function checkPasswords(group: FormGroup) {
     let pass = group.get('password').value;
     let confirmPass = group.get('password_confirmation').value;
-    return pass === confirmPass ? null : { notSame: true }
+    return pass === confirmPass ? null : { notSame: true };
 }
